99eb5077794395db2b5d00c036abeb8d6310da0f
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-08 21:11:30 -0300

    Finishing up the server reference to get the request and response to be listened over door 3333

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log("Hello hello heloo how low!")
+import * as http from "http";
+
+const server = http.createServer(
+    (request: http.IncomingMessage, response: http.ServerResponse) => {}
+);
+
+server.listen(3333, () => {
+    console.log("Server is running at http://localhost:3333/");
+})
