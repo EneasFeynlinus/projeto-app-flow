848edaaba006d766dd2ac610af9aa4cfa2a07f73
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-09 14:10:19 -0300

    Creating service layer, and passing out the data from podcast-controllers to a json file within repositories folder

--- src/controllers/podcasts-controller.ts
+++ src/controllers/podcasts-controller.ts
@@ -1,24 +1,13 @@
 import {IncomingMessage, ServerResponse} from 'http';
 
-export const getListEpisodes = async (req: IncomingMessage, res: ServerResponse) =>{
+import {serviceListEpisodes} from '../services/list-episodes-service';
+
+export const getListEpisodes = async (
+    req: IncomingMessage,
+    res: ServerResponse
+) =>{
+    const content = await serviceListEpisodes()
+
     res.writeHead(200,{'Content-Type': "application/json"});
-    res.end(JSON.stringify([
-        {
-            podcastName: "flow",
-            episode: "ESPECIAL TOLKIEN e SENHOR DOS ANEIS",
-            videoId: "o5di2HQIyhc",
-            cover: "https://i.ytimg.com/vi/o5di2HQIyhc/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLA5JkEUCwfomt75f7T88-QoSZI45g",
-            link: "https://www.youtube.com/watch?v=o5di2HQIyhc",
-            categories: ["Tolkien", "midle earth"]
-        },
-        {
-            podcastName: "flow",
-            episode: "NUNCA MAIS ERRE NOS SEUS TREINOS [+ Julio Balestrin]",
-            videoId: "vIRM1AC3kR4",
-            cover: "https://i.ytimg.com/vi/vIRM1AC3kR4/hqdefault.jpg?sqp=-oaymwEnCNACELwBSFryq4qpAxkIARUAAIhCGAHYAQHiAQoIGBACGAY4AUAB&rs=AOn4CLBJXLsW95xslto0hAzBD6xgcLyUNg",
-            link: "https://www.youtube.com/watch?v=vIRM1AC3kR4",
-            categories: ["saúde", "bodybuilder"]
-        },
-    
-    ]))
+    res.end(JSON.stringify(content));
 }
