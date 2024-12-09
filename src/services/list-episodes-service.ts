3c188319312695d62aae9ac00eade28e65dcbd77
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-09 16:07:39 -0300

    Finishing up, model files interface

--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,21 +1,9 @@
+import { repositoryPodcast } from "../repositories/podcasts-repository";
+
 export const serviceListEpisodes = async () => {
     // Implementação do método de listagem de episódios
     // Retorna uma lista de episódios
-    const data = [
-        {
-            podcastName: "flow",
-            episode: "ESPECIAL TOLKIEN e SENHOR DOS ANEIS",
-            videoId: "o5di2HQIyhc",
-            categories: ["Tolkien", "midle earth"]
-        },
-        {
-            podcastName: "flow",
-            episode: "NUNCA MAIS ERRE NOS SEUS TREINOS [+ Julio Balestrin]",
-            videoId: "vIRM1AC3kR4",
-            categories: ["saúde", "bodybuilder"]
-        },
-    
-    ];
+    const data = await repositoryPodcast();
 
     return data;
 }
