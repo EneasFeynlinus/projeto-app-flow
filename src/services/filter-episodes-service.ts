8a058c970b611c16d9631990f6de50012ad536af
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-10 22:36:16 -0300

    Finishing up refatoring floating texts and magic numbers, into .env files, enuns files and utils files as well

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,6 +1,7 @@
 import { repositoryPodcast } from "../repositories/podcasts-repository"
 
-export const serviceFilterEpisodes = async (podcasName: string) => {
+export const serviceFilterEpisodes = async (podcasName: string | undefined) => {
+    const queryString = podcasName?.split("?p=")[1] || "";
     const data = await repositoryPodcast(podcasName);
 
     return data
