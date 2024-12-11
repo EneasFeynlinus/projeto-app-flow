7981811743759bad9a76d130c8976d90941fb472
Eneas Junior <eneas.silva.jr@gmail.com>
2024-12-11 00:49:55 -0300

    Using the type FilterPodcastModel over list-episodes service and formating responseForamt with body and statusCode using standard DTO

--- src/services/filter-episodes-service.ts
+++ src/services/filter-episodes-service.ts
@@ -1,10 +1,10 @@
-import { FilterPodcastModel } from "../models/filter-podcast-models";
+import { PodcastTransferModel } from "../models/filter-podcast-models";
 import { repositoryPodcast } from "../repositories/podcasts-repository"
 import { StatusCode } from "../utils/status-code";
 
-export const serviceFilterEpisodes = async (podcasName: string | undefined): Promise<FilterPodcastModel> => {
+export const serviceFilterEpisodes = async (podcasName: string | undefined): Promise<PodcastTransferModel> => {
     //define a interface de retorno
-    let responseFormat: FilterPodcastModel = {
+    let responseFormat: PodcastTransferModel = {
         statusCode: 0,
         body: [],
     };
@@ -13,9 +13,10 @@ export const serviceFilterEpisodes = async (podcasName: string | undefined): Pro
     const data = await repositoryPodcast(queryString);
 
     //Verifico se tem conteudo
-    responseFormat.statusCode = data.length ? StatusCode.OK : StatusCode.NoContent;
-
-    responseFormat.body = data;
+    responseFormat = {
+        statusCode: data.length? StatusCode.OK : StatusCode.NoContent,
+        body: data,
+    }
 
     return responseFormat
 }
--- src/services/list-episodes-service.ts
+++ src/services/list-episodes-service.ts
@@ -1,7 +1,22 @@
+import { PodcastTransferModel } from "../models/filter-podcast-models";
 import { repositoryPodcast } from "../repositories/podcasts-repository";
+import { StatusCode } from "../utils/status-code";
 
-export const serviceListEpisodes = async () => {
+export const serviceListEpisodes = async (): Promise<PodcastTransferModel> => {
+    //Define o contrato
+    let responseFormat: PodcastTransferModel = {
+        statusCode: 0,
+        body: [],
+    };
+    
+    //Busco os dados
     const data = await repositoryPodcast();
 
-    return data;
+    //Verifico o tipo de resposta
+    responseFormat = {
+        statusCode: data.length? StatusCode.OK : StatusCode.NoContent,
+        body: data,
+    }
+
+    return responseFormat;
 }
